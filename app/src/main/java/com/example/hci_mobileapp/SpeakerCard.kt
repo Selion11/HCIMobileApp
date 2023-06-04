d095c1c5c2abc80ede31590d951c0335198138af
Guido De Caro <gdecaro@itba.edu.ar>
2023-06-04 14:23:35 -0300

    Speaker functuins begun

--- app/src/main/java/com/example/hci_mobileapp/SpeakerCard.kt
+++ app/src/main/java/com/example/hci_mobileapp/SpeakerCard.kt
@@ -1,16 +1,25 @@
 package com.example.hci_mobileapp
 
+import android.graphics.Paint.Align
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
@@ -22,7 +31,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,33 +50,46 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import javax.sql.RowSet
 
 data class speakerIcons(
-    @DrawableRes val speaker: Int,
-    @DrawableRes val play: Int,
-    @DrawableRes val next: Int,
-    @DrawableRes val prev: Int,
-    @DrawableRes val more: Int
+    @DrawableRes val speaker: Int = R.drawable.baseline_speaker_24,
+    @DrawableRes val play: Int = R.drawable.baseline_play_arrow_24,
+    @DrawableRes val next: Int = R.drawable.baseline_skip_next_24,
+    @DrawableRes val prev: Int = R.drawable.baseline_skip_previous_24 ,
+    @DrawableRes val more: Int = R.drawable.baseline_more_vert_24
 )
-val speakerRenderer = speakerIcons(
-                                            speaker = R.drawable.baseline_speaker_24,
-                                            prev = R.drawable.baseline_skip_previous_24,
-                                            play =  R.drawable.baseline_play_arrow_24,
-                                            next = R.drawable.baseline_skip_next_24,
-                                            more = R.drawable.baseline_more_vert_24
-                                    )
+
+data class speakerStringActions(
+    @StringRes val playlist: Int = R.string.playlist,
+    @StringRes val genre: Int = R.string.genre,
+)
+
+data class speakerGenres(
+    @StringRes val clasical: Int = R.string.classic,
+    @StringRes val dance: Int = R.string.dance,
+    @StringRes val country: Int = R.string.country,
+    @StringRes val pop: Int = R.string.pop,
+    @StringRes val rock: Int = R.string.rock,
+)
 @ExperimentalMaterial3Api
 @Composable
 fun SpeakerCard(
     name : String,
     modifier: Modifier = Modifier,
-   speakertoRend: speakerIcons = speakerRenderer){
+   speakertoRend: speakerIcons = speakerIcons(),
+    actions: speakerStringActions = speakerStringActions(),
+    genres: speakerGenres = speakerGenres()
+){
+    val dialogOpen = remember {
+        mutableStateOf(false)
+    }
     Surface(
         shape = MaterialTheme.shapes.small,
         modifier = modifier,
         border = BorderStroke(width = 2.dp, color = Color.Black),
-        onClick = {}
+        onClick = {dialogOpen.value = true}
     ) {
         Column(
             modifier = Modifier
@@ -134,12 +159,53 @@ fun SpeakerCard(
             }
         }
     }
+    if (dialogOpen.value){
+        Dialog(onDismissRequest = { dialogOpen.value = false }) {
+            Surface(
+                modifier = Modifier
+                    .width(350.dp)
+                    .height(150.dp),
+                shape = MaterialTheme.shapes.large,
+                tonalElevation = AlertDialogDefaults.TonalElevation
+            ) {
+                Column(modifier = Modifier.padding(16.dp),
+                ) {
+                    Row(horizontalArrangement = Arrangement.SpaceEvenly
+                    ){
+                       TextButton(onClick = { /*TODO*/ }
+                       ) {
+                           Icon(painter = painterResource(id = R.drawable.baseline_playlist_play_24),
+                               contentDescription = null)
+                           Text(text = stringResource(actions.playlist))
+                       }
+                        TextButton(onClick = { /*TODO*/ }
+                        ) {
+                            Icon(painter = painterResource(id = R.drawable.baseline_music_note_24),
+                                contentDescription = null)
+                            Text(text = stringResource(actions.genre))
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(24.dp))
+                    Row(verticalAlignment = Alignment.Bottom){
+                        TextButton(
+                                onClick = {
+                                    dialogOpen.value = false
+                                },
+                            ) {
+                                Text("Confirm")
+                            }
+                        }
+                    }
 
+            }
+        }
+        }
 }
-
+/*
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun CardPrev(){
-    SpeakerCard(name = "Speaker")
+    speakerDialog()
 }
+*/
